af4d3e9f5e2454e0305fefdf3e165424eedef018
HerculesGit <hercules.silva@dce.ufpb.br>
2019-05-12 12:46:48 -0300

    mudando para corecao do email

--- src/main/java/com/herco/cursomc/CursomcApplication.java
+++ src/main/java/com/herco/cursomc/CursomcApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CursomcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CursomcApplication.class, args);
+//		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 }
